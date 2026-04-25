472384cd43abba935c5844aa66380b5d812b9176
perugushravya14 <perugushravya14@gmail.com>
2026-04-25 18:30:17 +0530

    Update and rename task1.java to NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -5,29 +5,29 @@ public class NumberGame{
   {
     Random RandomNumber=new Random();
     Scanner s=new Scanner(System.in);
-    int NumbertoGuess=RandomNumber.nextInt(bound:100)+1;
+    int NumbertoGuess=RandomNumber.nextInt(100)+1;
     int userinput=0;
     int NumberofAttempts=0;
-    System.out.println(x:"Welcome to the Number Guessing Game.");
-    System.out.println(x:"Guess a Number between 1 To 100");
+    System.out.println("Welcome to the Number Guessing Game.");
+    System.out.println("Guess a Number between 1 To 100");
     while(userinput!=NumbertoGuess)
       {
-        System.out.println(x:"Enter the Guessed number");
+        System.out.println("Enter the Guessed number");
         userinput=s.nextInt();
         NumberofAttempts++;
         if(userinput<NumbertoGuess)
         {
-          System.out.println(x:"Too low.Try again.");
+          System.out.println("Too low.Try again.");
         }
         else if(userinput>NumbertoGuess)
         {
-          System.out.println(x:"Too high.Try again.");
+          System.out.println("Too high.Try again.");
         }
         else if(NumberofAttempts>10)
         {
-          System.out.println(x:"You have exceeded the maximum number of attempts.");
+          System.out.println("You have exceeded the maximum number of attempts.");
           System.out.println("The correct number was "+NumbertoGuess);
-          System.out.println(x:"Better luck next time.");
+          System.out.println("Better luck next time.");
           break;
         }
         else
